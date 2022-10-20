93bb08dc472eaa2dab8050bfb4e93bfd4b96b137
Lukas Lanzner <LanznerLukas@dotspace.dev>
2022-10-20 17:19:47 +0200

    improve DrawGraph

--- DrawGraph/src/DrawGraph.java
+++ DrawGraph/src/DrawGraph.java
@@ -1,57 +1,52 @@
 import processing.core.PApplet;
+
 public class DrawGraph extends PApplet {
+
+  int nValues = 100;
+  int[] yLin = new int[nValues];
+  int[] yPar = new int[nValues];
+  int stepY, stepX;
 
   public static void main(String[] args) {
     PApplet.main(DrawGraph.class);
   }
-
-  int nValues = 15;
-
-  int[] yLin = new int[nValues];
-  int[] yPar = new int[nValues];
-
-  int maxValue;
-  int stepY;
-  int stepX;
 
   public void settings() {
     size(1000, 1000);
   }
 
   public void setup() {
-
-    for (int i = 0; i < nValues; i++) {
-      yLin[i] = (int) (0.5 * i + 60);
-      yPar[i] = (int) Math.pow(i, 2);
-    }
-
     background(255);
 
-    if (max(yLin) < max(yPar)) {
-      maxValue = max(yPar);
-    } else maxValue = max(yLin);
+    // Generate Values
+    for (int i = 0; i < nValues; i++) {
+      yLin[i] = 4 * i + 60;
+      yPar[i] = (int) Math.pow(i, 2) / 10;
+    }
 
-    stepX = 980 / nValues;
-    stepY = 980 / maxValue;
-
-    print(stepX);
+    stepX = (width - 20) / nValues;
   }
 
   public void draw() {
-    translate(20, 980);
+    translate(20, height - 20);
     stroke(0);
+    strokeWeight(1);
 
-    line(0, 0, pixelWidth, 0); //X Axis
-    line(0, 0, 0, - pixelHeight); //Y Axis
+    line(0, 0, width - 20, 0); //X Axis
+    for (int i = 0; i < width; i += stepX)
+      line(i, 5, i, - 5);
+
+    line(0, 0, 0, - height - 20); //Y Axis
 
     for (int i = 0; i < nValues; i++) {
-      strokeWeight(2);
+      strokeWeight(4);
       stroke(0, 255, 0);
       point(i * stepX, - (yPar[i]));
       stroke(0, 0, 255);
-      point(i * stepX, -(yLin[i]));
+      point(i * stepX, - (yLin[i]));
       strokeWeight(1);
     }
+    noLoop();
   }
 
 }
